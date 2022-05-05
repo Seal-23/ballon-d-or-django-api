9053cdd66e1927bcb26782a446c4631cbf47cff4
danisper <u1802700@unimilitar.edu.co>
2022-05-05 00:09:12 -0500

    added cors and static variables

--- ballondor/settings.py
+++ ballondor/settings.py
@@ -16,7 +16,8 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 ENVIRONMENT = os.environ.get('DJANGO_ENV', 'local')
-
+CORS_ORIGIN_ALLOW_ALL = True
+STATIC_ROOT = 'static'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
