2a687580ca44cb4d2a0c6cd6570581ba2ee5bc20
danisper <u1802700@unimilitar.edu.co>
2022-05-10 20:44:21 -0500

    github and gcp8

--- batch/deploy.sh
+++ batch/deploy.sh
@@ -1,36 +1,34 @@
 #!/bin/sh
 gcloud compute ssh --zone "us-central1-c" "ballon-d-or-django-api"  --project "ballon-d-or-django-api-public"
-cd ..
-ls
-# export DJANGO_ENV=prod
-# export DATABASE_PASSWORD=$1
-# if [ -d "./ballon-d-or-django-api" ]
-# then
-#     cd ballon-d-or-django-api
-#     git pull origin main
-# else
-#     git clone https://github.com/Seal-23/ballon-d-or-django-api.git
-#     cd ballon-d-or-django-api
-# fi
-# if [ -d "./env" ]
-# then
-#     source env/bin/activate
-# else
-#     python3 -m venv env
-#     source env/bin/activate
-# fi
-# pip install -r requirements.txt
-# if ! [[ -d "./static" ]]
-# then
-#     python3 manage.py collectstatic
-# fi
-# python3 manage.py migrate
-# if [ -f "./ballondor.pid" ]
-# then
-#     while kill $(cat ./ballondor.pid); do
-#         sleep 1
-#     done
-# fi
-# gunicorn --bind 0.0.0.0:8000 --pid=./ballondor.pid ballondor.wsgi --daemon
+export DJANGO_ENV=prod
+export DATABASE_PASSWORD=$1
+if [ -d "./ballon-d-or-django-api" ]
+then
+    cd ballon-d-or-django-api
+    git pull origin main
+else
+    git clone https://github.com/Seal-23/ballon-d-or-django-api.git
+    cd ballon-d-or-django-api
+fi
+if [ -d "./env" ]
+then
+    source env/bin/activate
+else
+    python3 -m venv env
+    source env/bin/activate
+fi
+pip install -r requirements.txt
+if ! [[ -d "./static" ]]
+then
+    python3 manage.py collectstatic
+fi
+python3 manage.py migrate
+if [ -f "./ballondor.pid" ]
+then
+    while kill $(cat ./ballondor.pid); do
+        sleep 1
+    done
+fi
+gunicorn --bind 0.0.0.0:8000 --pid=./ballondor.pid ballondor.wsgi --daemon
 
 
